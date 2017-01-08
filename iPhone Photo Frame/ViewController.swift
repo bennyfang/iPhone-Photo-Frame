4cb30227a35748c8d7ec9c6eb1b8303eca16e0de
Benny Fang <Benny@bennys-macbook.local>
2017-01-08 10:38:36 -0800

    Initial Commit

---
+++ iPhone Photo Frame/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iPhone Photo Frame
+//
+//  Created by Benny Fang on 2017-01-08.
+//  Copyright © 2017 Benny Fang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
